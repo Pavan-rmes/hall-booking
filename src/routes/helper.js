52c37e118c9d2a600330bb661baa72009858d230
pavan <pavanguntupally12@gmail.com>
2022-03-11 10:15:19 +0530

    validations for booking the room

--- src/routes/helper.js
+++ src/routes/helper.js
@@ -6,19 +6,43 @@ async function createRoom(roomData){
     return response;
 }
 
-async function bookRoom(roomData){
-    const getRoom = await client.db("node-task2").collection("rooms").findOne({id:roomData.id})
-    if(getRoom){
-        const response = await client.db("node-task2").collection("booked").insertOne(roomData)
-        return response;
-    }
-    return {status:"Room dosen't exist"}
+function getTime(date,time){
+    return new Date(`${date} ${time}`)
 }
 
-async function checkRoomBooked(roomData){
-    const getRoomDataFromDb = await client.db("node-task2").collection("rooms").find({id:roomData.id}).toArray()
-    console.log(getRoomDataFromDb);
-    return true;
+async function bookRoom(roomData){
+    const getRoom = await client.db("node-task2").collection("rooms").findOne({roomid:roomData.roomid})
+    let startDate = new Date(`${roomData.date} ${roomData.start}`);let endDate = new Date(`${roomData.date} ${roomData.end}`)
+    if(!getRoom){
+        return {status:"Room dosen't exist"}
+    }
+    if(startDate.toString() === "Invalid Date" || endDate.toString() === "Invalid Date"){
+        return {status:"Invalid Date range"}
+    }
+    if(!roomData.customerName){
+        return {status:"Customer name is compulsory"}
+    }
+    if(await checkRoomBooked(roomData,startDate,endDate)){
+        return {status:"Room not available for booking at this time"};
+    }
+    const response = await client.db("node-task2").collection("booked").insertOne(roomData)
+    return response;
+}
+
+async function checkRoomBooked(roomData,startDate,endDate){
+    const getRoomDataFromDb = await client.db("node-task2").collection("booked").find({roomid:roomData.roomid}).toArray()
+    const isRoomBooked = getRoomDataFromDb.filter((room)=>{
+        if(startDate >= getTime(room.date,room.start) && startDate < getTime(room.date,room.end)){
+            return true;
+        }
+        else if(endDate > getTime(room.date,room.start) && endDate <= getTime(room.date,room.end)){
+            return true;
+        }
+        else{
+            return false
+        }
+    })
+    return isRoomBooked.length;
 }
 async function getAllRoomStatus(){
     const getRoomDataFromDb = await client.db("node-task2").collection("rooms").find().toArray()
--- src/routes/roomgen.js
+++ src/routes/roomgen.js
@@ -3,20 +3,16 @@ import {createRoom,checkRoomBooked,bookRoom,getAllRoomStatus} from "./helper.js"
 
 export const roomRoute = express.Router()
 
-roomRoute.post("/create",express.json(),(req,res)=>{
+roomRoute.post("/create",express.json(),async (req,res)=>{
     const roomData = req.body;
     // const roomName = roomData.name; 
-    const response = createRoom(roomData)
+    const response = await createRoom(roomData)
     res.send(response)
 })
 
-roomRoute.post("/book",(req,res)=>{
+roomRoute.post("/book",express.json(),async (req,res)=>{
     const roomData = req.body;
-    if(checkRoomBooked(roomData)){
-        res.send({status:"Room not available for booking at this time"});
-        return;
-    }
-    const response = bookRoom(roomData);
+    const response = await bookRoom(roomData);
     res.send(response)
 })
 
